05b421799c179e8b498752a02b2a359e93122536
abhitechno01 <abhi.techno01@gmail.com>
2019-12-13 22:31:03 +0530

    added player home page

--- player/views.py
+++ player/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from gameplay.models import Game
 # Create your views here.
 def home(request):
-	return render(request, "player/home.html")
+	view_data={'ngames':Game.objects.count()}
+	return render(request, "player/home.html", view_data)
