953d9e721d6e40ac4c485fb67e1541db506be17e
abhitechno01 <abhi.techno01@gmail.com>
2019-12-20 07:50:57 +0530

    sublime added anaconda

--- player/views.py
+++ player/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from gameplay.models import Game
 # Create your views here.
+
+
 def home(request):
-	view_data={'ngames':Game.objects.count()}
-	return render(request, "player/home.html", view_data)
+    view_data = {'ngames': Game.objects.count()}
+    return render(request, "player/home.html", view_data)
