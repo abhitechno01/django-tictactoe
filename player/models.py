a44ba11f4192052344150ecc5825806782406075
abhitechno01 <abhi.techno01@gmail.com>
2019-12-27 09:14:08 +0530

    ran migration for invitation model

--- player/models.py
+++ player/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Invitation(models.Model):
+    from_user = models.ForeignKey(
+        User, related_name="invitation_sent", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(
+        User, related_name="invitation_received", on_delete=models.CASCADE)
+    message = models.CharField(max_length=300)
+    timestamp = models.DateField(auto_now_add=True)
