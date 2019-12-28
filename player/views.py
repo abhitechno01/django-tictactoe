9d5056074ba8086189d3d2faeb42a2bdea17fedb
abhitechno01 <abhi.techno01@gmail.com>
2019-12-28 09:05:39 +0530

    validating invitation input

--- player/views.py
+++ player/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import InvitationForm
+from .models import Invitation
 from gameplay.models import Game
 # Create your views here.
 
@@ -16,6 +17,14 @@ def home(request):
 
 @login_required
 def new_invitation(request):
-    form = InvitationForm()
+
+    if request.method == "POST":
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
     view_data = {'form': form}
     return render(request, "player/new_invitation_form.html", view_data)
