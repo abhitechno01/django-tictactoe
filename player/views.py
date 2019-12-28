5726f5b6249f8be044393d067e44b102548ddeb1
abhitechno01 <abhi.techno01@gmail.com>
2019-12-28 08:20:21 +0530

    working on new invitation model form

--- player/views.py
+++ player/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from .forms import InvitationForm
 from gameplay.models import Game
 # Create your views here.
 
@@ -10,3 +12,10 @@ def home(request):
     active_games = my_games.active()
     view_data = {'games': active_games}
     return render(request, "player/home.html", view_data)
+
+
+@login_required
+def new_invitation(request):
+    form = InvitationForm()
+    view_data = {'form': form}
+    return render(request, "player/new_invitation_form.html", view_data)
